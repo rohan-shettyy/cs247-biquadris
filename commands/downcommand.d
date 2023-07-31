68ab39ba1782a8fed5853ae3e6f9b81263820f02
Kevin Gao <kevingao2003@gmail.com>
2023-07-31 18:28:08 -0400

    Delete downcommand.d

--- commands/downcommand.d
+++
@@ -1,3 +0,0 @@
-commands/downcommand.o: commands/downcommand.cc commands/../board.h \
- commands/../block.h commands/../level.h commands/../scoremanager.h \
- commands/downcommand.h commands/command.h
