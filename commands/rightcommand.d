6caa69ccb21f62b0a0e456b95822a22b07de7015
Kevin Gao <kevingao2003@gmail.com>
2023-07-31 18:26:56 -0400

    Delete rightcommand.d

--- commands/rightcommand.d
+++
@@ -1,3 +0,0 @@
-commands/rightcommand.o: commands/rightcommand.cc commands/../board.h \
- commands/../block.h commands/../level.h commands/../scoremanager.h \
- commands/rightcommand.h commands/command.h
